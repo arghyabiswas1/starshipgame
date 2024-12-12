ccc0c9a7d8dce80abad001ff0ff2d8481ea42924
Eric Ni <6600271@spsd.sk.ca>
2024-12-12 14:48:33 -0600

    spaceship 0.001
    Beta version of space fighter by Eric Ni and Arghya Biswas

--- spaceship_Eric.ino
+++ spaceship_Eric.ino
@@ -28,9 +28,7 @@ construction for this device:
 /*
 to do list:
 generate enemy bullet
-generate enemy ship
 rendering enemy bullet
-rendering enemy ship
 hitbox check
 */
 #include <Arduino.h>
@@ -69,6 +67,7 @@ static int x = 5;         //the current x position of the ship
 static int y = 29;        //the current y position of the ship
 static int speed_x = 0;   //the speed of the ship on x axis
 static int speed_y = 0;
+byte heroship_health = 10;
 
 //enemy ship
 static const int MAX_ENEMYSHIP = 5; //max ship amount for enemy ships
@@ -77,7 +76,8 @@ static byte EnemyX[MAX_ENEMYSHIP];
 static byte EnemyY[MAX_ENEMYSHIP];
 static bool enemyship_active[MAX_ENEMYSHIP];
 unsigned static long time_last_ship;
-
+static byte enemy_speed[MAX_ENEMYSHIP];
+static byte enemy_ticking_counter[MAX_ENEMYSHIP];
 
 //bullet
 static const int MAX_BULLETS = 20;
@@ -85,24 +85,15 @@ static byte bulletX[MAX_BULLETS];
 static byte bulletY[MAX_BULLETS];
 static bool bulletActive[MAX_BULLETS];
 unsigned static long time_last_bullet;
-static byte bullet_activated;
 
 static const unsigned char spaceship[] U8X8_PROGMEM  = {  //the spaceship
   0x06, 0x00, 0x1E, 0x00, 0x7E, 0x00, 0xFF, 0x03, 0xFF, 0x0C, 0xFF, 0x03, 0x7E, 0x00, 0x1E, 0x00, 0x06, 0x00,
 };
-/*
-const unsigned char enemyship2 [] PROGMEM = {
-    0xff, 0x88, 0xff, 0x18, 0xfe, 0x18, 0xf8, 0x08, 0xc0, 0x80, 0x39, 0x00, 0xc0, 0x80, 0xf8, 0x08, 0xfe, 0x18, 0xff, 0x18, 0xff, 0x88,
-};
-*/
 
-const unsigned char enemyship2 [] PROGMEM = {
+const unsigned char enemyship2[] U8X8_PROGMEM = {
     0x00, 0x0E, 0x00, 0x07, 0x80, 0x07, 0xE0, 0x0F, 0x7C, 0x1F, 0x63, 0x1F, 0x7C, 0x1F, 0xE0, 0x0F, 0x80, 0x07, 0x00, 0x07, 0x00, 0x0E,
 };
 
-//debug print
-//static String x_status;
-//static String y_status;
 
 /**************************************************************/
 
@@ -156,34 +147,24 @@ void spaceship_Full(){
   y += speed_y;
   x += speed_x;
   u8g2.clearBuffer();
-  u8g2.drawStr(5, 18, "shipbattle");
+  u8g2.drawStr(5, 18, "SpaceFighter");
   u8g2.drawXBMP(x, y, 12, 9, spaceship);
 
   //rendering bullet
-  for(int i = 0; i < bullet_activated; i++){
-    bulletX[i] += 2;
-    if(!bulletActive[i]){continue;}
-    if(bulletX[i] > 128){
-      bulletActive[i] = false;
-      bullet_activated--;
-      continue;
+  for(int i = 0; i < MAX_BULLETS; i++){
+    if(bulletActive[i]){
+      u8g2.drawPixel(bulletX[i], bulletY[i]);     // First pixel
+      u8g2.drawPixel(bulletX[i] + 1, bulletY[i]); // Second pixel
     }
-    u8g2.drawPixel(bulletX[i], bulletY[i]);     // First pixel
-    u8g2.drawPixel(bulletX[i] + 1, bulletY[i]); // Second pixel
   }
 
   //rendering enemy bullet
 
   //rendering enemyship
   for(int i = 0; i < MAX_ENEMYSHIP; i++){
-    EnemyX[i] -= 1;
-    if(!enemyship_active[i]){continue;}
-    if(EnemyX[i] < 0){
-      enemyship_active[i] = false;
-      continue;
+    if(enemyship_active[i]){
+      if(ENEMY_VARIENT[i] == 2){u8g2.drawXBMP(EnemyX[i], EnemyY[i], 13, 11, enemyship2);}
     }
-
-    if(ENEMY_VARIENT[i] == 2){u8g2.drawXBMP(EnemyX[i], EnemyY[i], 13, 11, enemyship2);}
   }
 
   u8g2.sendBuffer();
@@ -197,19 +178,19 @@ void joystick_data_receive(){
   y_value = analogRead(A1);
 
   if (x_value > x_mid + dead_zone && right_allowed){ // right (plus)
-    speed_x = map(x_value, x_mid + dead_zone, x_max, 1, 6);
+    speed_x = map(x_value, x_mid + dead_zone, x_max, 1, 3);
   }else if (x_value < x_mid - dead_zone && left_allowed) { // left (minus)
-    speed_x = map(x_value, x_mid - dead_zone, 0, 1, 6);
+    speed_x = map(x_value, x_mid - dead_zone, 0, 1, 3);
     speed_x = speed_x * -1;
   }else { // middle (plus)
     speed_x = 0;
   }
 
   if (y_value > y_mid + dead_zone && up_allowed){ // up (minus)
-    speed_y = map(y_value, y_mid + dead_zone, y_max, 1, 6);
+    speed_y = map(y_value, y_mid + dead_zone, y_max, 1, 3);
     speed_y = speed_y * -1;
   }else if (y_value < x_mid - dead_zone && down_allowed){ // down (plus)
-    speed_y = map(y_value, y_mid - dead_zone, 0, 1, 6);
+    speed_y = map(y_value, y_mid - dead_zone, 0, 1, 3);
   }else { // middle
     speed_y = 0;
   } 
@@ -227,23 +208,37 @@ void detect_Border() {
 /**************************************************************/
 
 void bullet(){
+  //generate bullet
   unsigned long time_now = millis();
-  if(!digitalRead(2) & time_now - time_last_bullet >= 500 & bullet_activated < MAX_BULLETS){
+  if(!digitalRead(2) & time_now - time_last_bullet >= 500){
     for (int i = 0; i <= MAX_BULLETS; i++) {
       if(!bulletActive[i]){
         bulletX[i] = x + 12; // Start at the tip of the spaceship
         bulletY[i] = y + 4;  // Center vertically
         bulletActive[i] = true;
-        bullet_activated ++;
         time_last_bullet = time_now;
+        /*
         Serial.print("bullet_activated: ");
         Serial.println(bullet_activated);
         Serial.print("i: ");
         Serial.println(i);
         Serial.print(bulletX[i]);
         Serial.println(bulletY[i]);
+        */
         break;
+        
       }
+    }
+  }
+
+  //update and recycle bullet
+  for(int i = 0; i < MAX_BULLETS; i++){
+    if(!bulletActive[i]){continue;}
+    bulletX[i] += 2;
+    //if(!bulletActive[i]){continue;}
+    if(bulletX[i] > 128){
+      bulletActive[i] = false;
+      continue;
     }
   }
 }
@@ -251,18 +246,46 @@ void bullet(){
 /**************************************************************/
 
 void enemyShip_Logics(){
+  //generate enemies
   unsigned long time_now = millis();
-  if(time_now - time_last_ship >= 10000){
+  if(time_now - time_last_ship >= 5000){
     for (int i = 0; i <= MAX_ENEMYSHIP; i++) {
       if(!enemyship_active[i]){
         //ENEMY_VARIENT[i] = random(1, 4);
         ENEMY_VARIENT[i] = 2;
         EnemyX[i] = 128;
-        EnemyY[i] = random(1, 64);
+        EnemyY[i] = random(1, 55);
         enemyship_active[i] = true;
         time_last_ship = time_now;
         break;
       }
+    }
+  }
+
+  //Enemy_actions
+  for(int i = 0; i < MAX_ENEMYSHIP; i++){
+    if(!enemyship_active[i]){continue;}
+    byte distance_to_hero = EnemyX[i] - x;
+    byte enemyship_center = EnemyY[i] + 5;
+    //Enemy_actions
+    if(distance_to_hero - x < 30 & 0 < distance_to_hero & enemyship_center > y & enemyship_center < y + 8){ //if Enemy is -5 to 20 pixel in front of hero
+      if(enemy_ticking_counter[i] > 5){
+        enemy_speed[i] = 1;
+        enemy_ticking_counter[i] = 0;
+      }else{
+        enemy_speed[i] = 0;
+        enemy_ticking_counter[i]++;
+      }
+    }else{
+      enemy_speed[i] = 1;
+    }
+
+    //recycle enemies    
+    EnemyX[i] -= enemy_speed[i];
+    //if(!enemyship_active[i]){continue;}
+    if(EnemyX[i] <= 0){
+      enemyship_active[i] = false;
+      continue;
     }
   }
 }
@@ -270,7 +293,18 @@ void enemyShip_Logics(){
 /**************************************************************/
 
 void hitBox(){
+  //heroship hitbox:(smaller than the ship because the ship is in inregular shape)
+  byte right_hitbox = x + 8;
+  byte left_hitbox = x + 1;
+  byte top_hitbox = y + 1;
+  byte down_hitbox = y + 7;
+
   //if heroship collide with bullet
+  for(int i = 0; i < MAX_BULLETS; i++){
+    if(bulletActive[i] & bulletX[i] <= right_hitbox & bulletX[i] >= left_hitbox & bulletY[i] <= down_hitbox & bulletY[i] >= top_hitbox){
+      heroship_health --;
+    }
+  }
 
   //if heroship collide with enemyship
 
@@ -305,9 +339,4 @@ Enemyship2
 10000000 00000111
 00000000 00000111
 00000000 00001110
-
-const unsigned char enemyship2 [] PROGMEM = {
-    0x00, 0x0E, 0x00, 0x07, 0x80, 0x07, 0xE0, 0x0F, 0x7C, 0x1F, 0x63, 0x1F, 0x7C, 0x1F, 0xE0, 0x0F, 0x80, 0x07, 0x00, 0x07, 0x00, 0x0E,
-};
-
 */
